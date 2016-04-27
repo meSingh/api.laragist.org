71d7a29d1e45a17491e055a4f99cdfb730429885
Piyush Agrawal <poush12@gmail.com>
2016-04-27 18:17:28 +0530

    Transformer added for package view
    Signed-off-by: Piyush Agrawal <poush12@gmail.com>

--- app/Http/Controllers/v1/PackageController.php
+++ app/Http/Controllers/v1/PackageController.php
@@ -20,6 +20,7 @@ use GistApi\Repositories\Author;
 use GistApi\Repositories\Package;
 use GistApi\Repositories\PackageRepo;
 use GistApi\Transformers\PackagesTransformer;
+use GistApi\Transformers\PackageTransformer;
 
 use GistApi\Http\Controllers\v1\ApiController;
 
@@ -188,8 +189,7 @@ class PackageController extends ApiController
                                     ->where('version', $package->version)
                                     ->first();
         }
-        
-        return $package;
+        return $this->item($package, new PackageTransformer);
     }
 
 }
