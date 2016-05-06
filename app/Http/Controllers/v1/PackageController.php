59a0f7c24794c110fa19140e12aff6751a62bac4
Mandeep Singh <im@msingh.me>
2016-05-06 22:56:45 +0530

    testing revert
    Signed-off-by: Mandeep Singh <im@msingh.me>

--- app/Http/Controllers/v1/PackageController.php
+++ app/Http/Controllers/v1/PackageController.php
@@ -72,7 +72,7 @@ class PackageController extends ApiController
                             ->where('package_categories.category_id',$request->get('cid'));
             
                             
-        $packages = $packages->paginate(2);             
+        $packages = $packages->paginate(20);             
 
         return $this->response->paginator($packages, new PackagesTransformer);
 
